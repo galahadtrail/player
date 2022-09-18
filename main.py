113b4c565d7c44782378e4641751022fe02dc502
galahadtrail <zzzzrzzzz@yandex.ru>
2022-09-18 23:03:57 +0300

    added trailing button

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.button_is_checked = True
+
         self.setWindowTitle("My App")
         button = QPushButton("Press Me!")
         button.setCheckable(True)
         button.clicked.connect(self.the_button_was_clicked)
         button.clicked.connect(self.the_button_was_toggled)
+        button.setChecked(self.button_is_checked)
 
         self.setFixedSize(400, 300)
 
@@ -20,9 +23,9 @@ class MainWindow(QMainWindow):
     def the_button_was_clicked(self):
         print("Clicked!")
 
-    @staticmethod
     def the_button_was_toggled(self):
-        print('Checked?')
+        print(self.button_is_checked)
+        self.button_is_checked = False if self.button_is_checked else True
 
 
 app = QApplication([])
