9a5b4697b62efa2b2cfc472e8fb13686331008c4
galahadtrail <zzzzrzzzz@yandex.ru>
2022-09-19 17:43:30 +0300

    beatuify code

--- player_form.py
+++ player_form.py
@@ -1,47 +1,52 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtWidgets import QApplication, QMainWindow
 
 
-class Ui_WizardPage(object):
+class Uiwizard_page(object):
 
-    def setupUi(self, WizardPage):
-        WizardPage.setObjectName("WizardPage")
-        WizardPage.resize(652, 596)
-        WizardPage.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-        WizardPage.setAutoFillBackground(False)
-        self.scrollArea = QtWidgets.QScrollArea(WizardPage)
-        self.scrollArea.setGeometry(QtCore.QRect(90, 20, 481, 331))
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollArea.setObjectName("scrollArea")
-        self.scrollAreaWidgetContents = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 479, 329))
-        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-        self.pushButton = QtWidgets.QPushButton(WizardPage)
-        self.pushButton.setGeometry(QtCore.QRect(290, 370, 89, 25))
+    def setup_ui(self, wizard_page):
+        wizard_page.setObjectName("wizard_page")
+        wizard_page.resize(652, 596)
+        wizard_page.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        wizard_page.setAutoFillBackground(False)
+
+        self.scroll_area = QtWidgets.QScrollArea(wizard_page)
+        self.scroll_area.setGeometry(QtCore.QRect(90, 20, 481, 331))
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setObjectName("scroll_area")
+
+        self.scroll_area_widget_contents = QtWidgets.QWidget()
+        self.scroll_area_widget_contents.setGeometry(QtCore.QRect(0, 0, 479, 329))
+        self.scroll_area_widget_contents.setObjectName("scroll_area_widget_contents")
+        self.scroll_area.setWidget(self.scroll_area_widget_contents)
+        self.push_button_1 = QtWidgets.QPushButton(wizard_page)
+
+        self.push_button_1.setGeometry(QtCore.QRect(290, 370, 89, 25))
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("images/pause.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.pushButton.setIcon(icon)
-        self.pushButton.setCheckable(True)
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(WizardPage)
-        self.pushButton_2.setGeometry(QtCore.QRect(390, 370, 89, 25))
-        self.pushButton_2.setText("")
+        self.push_button_1.setIcon(icon)
+        self.push_button_1.setCheckable(True)
+        self.push_button_1.setObjectName("pause")
+
+        self.push_button_2 = QtWidgets.QPushButton(wizard_page)
+        self.push_button_2.setGeometry(QtCore.QRect(390, 370, 89, 25))
+        self.push_button_2.setText("")
         icon1 = QtGui.QIcon()
         icon1.addPixmap(QtGui.QPixmap("images/forward_button.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.pushButton_2.setIcon(icon1)
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_3 = QtWidgets.QPushButton(WizardPage)
-        self.pushButton_3.setGeometry(QtCore.QRect(190, 370, 89, 25))
-        self.pushButton_3.setText("")
+        self.push_button_2.setIcon(icon1)
+        self.push_button_2.setObjectName("forward")
+
+        self.push_button_3 = QtWidgets.QPushButton(wizard_page)
+        self.push_button_3.setGeometry(QtCore.QRect(190, 370, 89, 25))
+        self.push_button_3.setText("")
         icon2 = QtGui.QIcon()
         icon2.addPixmap(QtGui.QPixmap("images/backward_button.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.pushButton_3.setIcon(icon2)
-        self.pushButton_3.setObjectName("pushButton_3")
+        self.push_button_3.setIcon(icon2)
+        self.push_button_3.setObjectName("backward")
 
-        self.retranslateUi(WizardPage)
-        QtCore.QMetaObject.connectSlotsByName(WizardPage)
+        self.retranslate_ui(wizard_page)
+        QtCore.QMetaObject.connectSlotsByName(wizard_page)
 
-    def retranslateUi(self, WizardPage):
+    @staticmethod
+    def retranslate_ui(self, wizard_page):
         _translate = QtCore.QCoreApplication.translate
-        WizardPage.setWindowTitle(_translate("WizardPage", "WizardPage"))
+        wizard_page.setWindowTitle(_translate("wizard_page", "wizard_page"))
