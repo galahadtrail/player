cff5bd2d50e1ff230f94964b49f8a8d397abd132
galahadtrail <zzzzrzzzz@yandex.ru>
2022-09-19 17:50:15 +0300

    code refactoring, window can be opened in this version

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from player_form import UiWizardPage
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
-
-        self.button_is_checked = True
-
-        self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
-        button.setFixedSize(200, 200)
-        button.setCheckable(True)
-        button.clicked.connect(self.the_button_was_clicked)
-        button.clicked.connect(self.the_button_was_toggled)
-        button.setChecked(self.button_is_checked)
-
-        self.setFixedSize(400, 300)
-
-        self.setCentralWidget(button)
-
-    @staticmethod
-    def the_button_was_clicked(self):
-        print("Clicked!")
-
-    def the_button_was_toggled(self):
-        print(self.button_is_checked)
-        self.button_is_checked = False if self.button_is_checked else True
+        super(MainWindow, self).__init__()
+        self.ui = UiWizardPage()
+        self.ui.setup_ui(self)
 
 
 app = QApplication([])
--- player_form.py
+++ player_form.py
@@ -1,12 +1,11 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
 
 
-class Uiwizard_page(object):
+class UiWizardPage(object):
 
     def setup_ui(self, wizard_page):
         wizard_page.setObjectName("wizard_page")
         wizard_page.resize(652, 596)
-        wizard_page.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         wizard_page.setAutoFillBackground(False)
 
         self.scroll_area = QtWidgets.QScrollArea(wizard_page)
@@ -22,7 +21,7 @@ class Uiwizard_page(object):
 
         self.push_button_1.setGeometry(QtCore.QRect(290, 370, 89, 25))
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("images/pause.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("images/pause.png"))
         self.push_button_1.setIcon(icon)
         self.push_button_1.setCheckable(True)
         self.push_button_1.setObjectName("pause")
@@ -31,7 +30,7 @@ class Uiwizard_page(object):
         self.push_button_2.setGeometry(QtCore.QRect(390, 370, 89, 25))
         self.push_button_2.setText("")
         icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap("images/forward_button.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon1.addPixmap(QtGui.QPixmap("images/forward_button.png"))
         self.push_button_2.setIcon(icon1)
         self.push_button_2.setObjectName("forward")
 
@@ -39,7 +38,7 @@ class Uiwizard_page(object):
         self.push_button_3.setGeometry(QtCore.QRect(190, 370, 89, 25))
         self.push_button_3.setText("")
         icon2 = QtGui.QIcon()
-        icon2.addPixmap(QtGui.QPixmap("images/backward_button.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon2.addPixmap(QtGui.QPixmap("images/backward_button.png"))
         self.push_button_3.setIcon(icon2)
         self.push_button_3.setObjectName("backward")
 
@@ -47,6 +46,6 @@ class Uiwizard_page(object):
         QtCore.QMetaObject.connectSlotsByName(wizard_page)
 
     @staticmethod
-    def retranslate_ui(self, wizard_page):
+    def retranslate_ui(wizard_page):
         _translate = QtCore.QCoreApplication.translate
         wizard_page.setWindowTitle(_translate("wizard_page", "wizard_page"))
